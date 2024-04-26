a340c1bad6b28d543d4c8c8579b6fb209adc99fb
Dewansh lowanshi <147435337+Dewansh99@users.noreply.github.com>
2024-04-26 13:25:41 +0530

    test sumbitted

--- program1.js
+++ program1.js
@@ -2,6 +2,19 @@ function smallestMissingPositiveInteger(nums) {
 
 
     // Implement the function smallest_missing_positive_integer
+    let newArray = A.sort(function(a, b){return a-b})
+        let lowestNumber = 1
+        for(i=0; i < newArray.length; i++) {
+            if(lowestNumber > newArray[0]) {
+                return lowestNumber
+            }
+            if(lowestNumber == newArray[i]) {
+                lowestNumber = lowestNumber + 1
+            }
+            if(i = newArray.length - 1) {
+                return lowestNumber
+            }  
+        }
   
   }
   
--- program2.js
+++ program2.js
@@ -2,6 +2,30 @@ function longestSubstring(s) {
 
 
     // Implementation of longestSubstring function
-}
+    var n = s.length;
+
+        var res = 0; 
+
+        
+        let lastIndex = new Array(256).fill(-1);
+
+        var i = 0;
+
+     
+        for (var j = 0; j < n; j++) {
+
+           
+            i = Math.max(i, lastIndex[s.charAt(j)] + 1);
+
+            
+            res = Math.max(res, j - i + 1);
+
+            
+            lastIndex[s.charAt(j)] = j;
+        }
+        return res;
+
+    }  
+
 
 module.exports = { longestSubstring };
